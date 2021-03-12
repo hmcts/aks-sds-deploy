ef85352d060f51439a0b3d0b9424b6c4dde4da14
Luigi Bitonti <luigi.bitonti@hmcts.net>
2021-03-12 13:26:33 +0000

    DTSPO-1644 default + additional routes

--- terraform/aks/01-network/20-network.tf
+++ terraform/aks/01-network/20-network.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source = "git::https://github.com/hmcts/aks-module-network.git?ref=master"
+  source = "git::https://github.com/hmcts/aks-module-network.git?ref=DTSPO-1644-add-route-resource"
 
   resource_group_name = local.network_resource_group_name
 
