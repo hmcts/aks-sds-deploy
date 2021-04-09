ce1d12df7fa6d42c3522fce371419e7ac80708e6
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2021-04-09 16:35:51 +0100

    add test additional node

--- terraform/aks/03-aks00/20-aks.tf
+++ terraform/aks/03-aks00/20-aks.tf
@@ -49,4 +49,15 @@ module "kubernetes" {
   kubernetes_cluster_version         = var.kubernetes_cluster_version
 
   tags = local.common_tags
+
+  additional_node_pools = {
+    name                  = "msnode"
+    kubernetes_cluster_id = data.azurerm_subnet.aks.id
+    min_count             = var.kubernetes_cluster_agent_min_count
+    max_count             = var.kubernetes_cluster_agent_max_count
+    os_type               = "Windows"
+    os_disk_type          = "Ephemeral"
+    node_taints           = ["kubernetes.io/os=windows:NoSchedule"]
+  }
+
 }
