3d6707a9f68300deaaafe6084f161fc5b881dd1a
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2021-04-27 16:52:15 +0100

    Add developers_group to 10-genesis.tf

--- terraform/aks/00-genesis/10-genesis.tf
+++ terraform/aks/00-genesis/10-genesis.tf
@@ -1,5 +1,6 @@
 module "genesis" {
-  source      = "git::https://github.com/hmcts/aks-module-genesis.git?ref=master"
-  environment = var.environment
-  tags        = local.common_tags
+  source           = "git::https://github.com/hmcts/aks-module-genesis.git?ref=master"
+  environment      = var.environment
+  tags             = local.common_tags
+  developers_group = var.developers_group
 }
