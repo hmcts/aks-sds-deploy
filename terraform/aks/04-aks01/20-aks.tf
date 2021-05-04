ec50d4fe4977e14c3623c82f9c1c0f64364286c9
mokainos <72137340+mokainos@users.noreply.github.com>
2021-05-04 10:07:47 +0100

    pointing to master branch

--- terraform/aks/04-aks01/20-aks.tf
+++ terraform/aks/04-aks01/20-aks.tf
@@ -15,7 +15,7 @@ module "loganalytics" {
 }
 
 module "kubernetes" {
-  source = "git::https://github.com/hmcts/aks-module-kubernetes.git?ref=dtspo-2396"
+  source = "git::https://github.com/hmcts/aks-module-kubernetes.git?ref=master"
 
   environment = var.environment
   location    = var.location
