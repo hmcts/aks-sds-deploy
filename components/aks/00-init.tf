98a63aca436a11aa53343039d79defd0fd069002
msl8r <72858053+msl8r@users.noreply.github.com>
2021-06-02 09:32:59 +0100

    DTSPO-1032 - single aks configuration

---
+++ components/aks/00-init.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_version = ">= 0.12.0"
+
+  backend "azurerm" {
+    subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.57.0"
+      configuration_aliases = [ azurerm.hmcts-control ]
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  acr = {
+    ss = {
+      subscription = "5ca62022-6aa2-4cee-aaa7-e7536c8d566c"
+    }
+    global = {
+      subscription = "8999dec3-0104-4a27-94ee-6588559729d1"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id            = local.acr[var.project].subscription
+  skip_provider_registration = "true"
+  features {}
+  alias = "acr"
+}
+
+provider "azurerm" {
+  subscription_id            = local.acr["global"].subscription
+  skip_provider_registration = "true"
+  features {}
+  alias = "global_acr"
+}
+
+provider "azurerm" {
+  alias                      = "hmcts-control"
+  skip_provider_registration = "true"
+  features {}
+  subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
+}
---
+++ components/aks/20-aks.tf
@@ -0,0 +1,70 @@
+resource "azurerm_resource_group" "kubernetes_resource_group" {
+  count = 2
+  location = var.location
+
+  name = format("%s-%s-%s-rg",
+    var.project,
+    var.environment,
+    "0${count.index}"
+  )
+  tags = local.common_tags
+}
+
+module "loganalytics" {
+  source      = "git::https://github.com/hmcts/terraform-module-log-analytics-workspace-id.git?ref=master"
+  environment = var.environment
+}
+
+module "kubernetes" {
+  count = 2
+  # source = "git::https://github.com/hmcts/aks-module-kubernetes.git?ref=DTSPO-1032_multi_cluster"
+  source = "../../../../tf_modules/aks-module-kubernetes"
+  environment = var.environment
+  location    = var.location
+
+  providers = {
+    azurerm               = azurerm
+    azurerm.hmcts-control = azurerm.hmcts-control
+    azurerm.acr           = azurerm.acr
+    azurerm.global_acr    = azurerm.global_acr
+  }
+
+  resource_group_name = azurerm_resource_group.kubernetes_resource_group[count.index].name
+
+  network_name                = local.network_name
+  network_shortname           = local.network_shortname
+  network_resource_group_name = local.network_resource_group_name
+
+  cluster_number    = "0${count.index}"
+  service_shortname = var.service_shortname
+  project           = var.project
+
+  log_workspace_id = module.loganalytics.workspace_id
+
+  control_vault = var.control_vault
+
+  kubernetes_cluster_ssh_key = var.kubernetes_cluster_ssh_key
+
+  kubernetes_cluster_agent_min_count    = var.kubernetes_cluster_agent_min_count
+  kubernetes_cluster_agent_max_count    = var.kubernetes_cluster_agent_max_count
+  kubernetes_cluster_agent_vm_size      = var.kubernetes_cluster_agent_vm_size
+  kubernetes_cluster_version            = var.kubernetes_cluster_version
+  kubernetes_cluster_agent_os_disk_size = "128"
+
+  tags = local.common_tags
+
+  # cluster_count = "2"
+
+
+  additional_node_pools = [
+    {
+      name                = "msnode"
+      vm_size             = var.kubernetes_cluster_agent_vm_size
+      min_count           = 2
+      max_count           = 5
+      os_type             = "Windows"
+      node_taints         = ["kubernetes.io/os=windows:NoSchedule"]
+      enable_auto_scaling = true
+    }
+  ]
+}
