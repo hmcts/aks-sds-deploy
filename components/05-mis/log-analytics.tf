b823ed347d6c9f1f5f22f38804000578af67e4ee
msl8r <72858053+msl8r@users.noreply.github.com>
2021-06-04 10:28:54 +0100

    DTSPO-1032 - use log analytics branch with ptl env

--- components/05-mis/log-analytics.tf
+++ components/05-mis/log-analytics.tf
@@ -1,20 +1,20 @@
 resource "azurerm_role_assignment" "log_analytics_role_MI" {
 
-  provider = azurerm.log_analytics
-  principal_id                    = azurerm_user_assigned_identity.sops-mi.principal_id
-  role_definition_name            = "Log Analytics Contributor"
-  scope                           = module.loganalytics.workspace_id
+  provider             = azurerm.log_analytics
+  principal_id         = azurerm_user_assigned_identity.sops-mi.principal_id
+  role_definition_name = "Log Analytics Contributor"
+  scope                = module.loganalytics.workspace_id
 }
 
 resource "azurerm_role_assignment" "log_analytics_role_aks_sp" {
   # AKS SP ACR Pull role
-  provider = azurerm.log_analytics
-  role_definition_name             = "Log Analytics Contributor"
-  principal_id                     = data.azurerm_key_vault_secret.kubernetes_cluster_client_id.value
-  scope                            = module.loganalytics.workspace_id
+  provider             = azurerm.log_analytics
+  role_definition_name = "Log Analytics Contributor"
+  principal_id         = data.azurerm_key_vault_secret.kubernetes_cluster_client_id.value
+  scope                = module.loganalytics.workspace_id
 }
 
 module "loganalytics" {
-  source      = "git::https://github.com/hmcts/terraform-module-log-analytics-workspace-id.git?ref=master"
+  source      = "git::https://github.com/hmcts/terraform-module-log-analytics-workspace-id.git?ref=DTSPO-1032_add_ptl_envs"
   environment = var.environment
 }
