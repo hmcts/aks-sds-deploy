d769a305d0d2fa0b8ddf8dac12fcb08430f182af
Tim Jacomb <tim.jacomb@hmcts.net>
2021-06-21 11:47:10 +0100

    Fix typoes

--- components/05-mis/03-interpolated-defaults.tf
+++ components/05-mis/03-interpolated-defaults.tf
@@ -7,7 +7,7 @@ data "azurerm_resource_group" "genesis_rg" {
 }
 
 data "azurerm_resource_group" "platform-rg" {
-  name = "sds-platform-${var.env}-rg"
+  name = "sds-platform-${var.environment}-rg"
 }
 
 data "azurerm_key_vault" "genesis_keyvault" {
@@ -28,6 +28,6 @@ data "azurerm_key_vault_secret" "kubernetes_cluster_client_id" {
 }
 
 data "azurerm_key_vault" "acme" {
-  name = "acmedtssds${var.env}"
+  name = "acmedtssds${var.environment}"
   resource_group_name = data.azurerm_resource_group.sds-platform-sbox-rg.name
 }
