737d989d5abd03a624ea84d444793bc97815d499
Enda Kelly <enda.kelly@hmcts.net>
2021-06-24 09:23:47 +0100

    Adding resource group for disks

--- components/aks/aks.tf
+++ components/aks/aks.tf
@@ -10,6 +10,12 @@ resource "azurerm_resource_group" "kubernetes_resource_group" {
   tags = module.ctags.common_tags
 }
 
+resource "azurerm_resource_group" "disks_resource_group" {
+  location = var.location
+  name = "disks-${var.environment}-rg"
+  tags = module.ctags.common_tags
+}
+
 module "loganalytics" {
   source      = "git::https://github.com/hmcts/terraform-module-log-analytics-workspace-id.git?ref=master"
   environment = var.environment
