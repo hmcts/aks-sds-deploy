55c6346ee21748d7cb13f9c21441eba4fd319f84
rahman124 <rahman124@live.com>
2021-07-22 13:48:15 +0100

    add network init back

---
+++ components/01-network/00-init.tf
@@ -0,0 +1,78 @@
+terraform {
+  required_version = ">= 0.12.0"
+
+  backend "azurerm" {
+    subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.57.0"
+    }
+  }
+}
+
+variable "subscription_id" {}
+
+provider "azurerm" {
+  features {}
+  subscription_id            = var.subscription_id
+  skip_provider_registration = "true"
+}
+
+provider "azurerm" {
+  alias                      = "hmcts-control"
+  skip_provider_registration = "true"
+  features {}
+  subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
+}
+
+# Need to leave in until apply has been made in each env
+provider "azurerm" {
+  subscription_id            = local.hub[var.hub].subscription
+  skip_provider_registration = "true"
+  features {}
+  alias = "hub"
+}
+
+provider "azurerm" {
+  subscription_id            = local.hub["sbox"].subscription
+  skip_provider_registration = "true"
+  features {}
+  alias = "hub-sbox"
+}
+
+provider "azurerm" {
+  subscription_id            = local.hub["nonprod"].subscription
+  skip_provider_registration = "true"
+  features {}
+  alias = "hub-nonprod"
+}
+
+provider "azurerm" {
+  subscription_id            = local.hub["prod"].subscription
+  skip_provider_registration = "true"
+  features {}
+  alias = "hub-prod"
+}
+
+provider "azurerm" {
+  subscription_id            = var.private_dns_subscription
+  skip_provider_registration = "true"
+  features {}
+  alias = "private-dns"
+}
+
+provider "azurerm" {
+  subscription_id            = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
+  skip_provider_registration = "true"
+  features {}
+  alias = "private-dns-private-endpoint"
+}
+
+provider "azurerm" {
+  subscription_id            = "ed302caf-ec27-4c64-a05e-85731c3ce90e"
+  skip_provider_registration = "true"
+  features {}
+  alias = "vpn"
+}
