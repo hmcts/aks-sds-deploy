a7ebac34cd806f1510782207d822bf7ca31c6437
msl8r <72858053+msl8r@users.noreply.github.com>
2021-09-01 12:02:12 +0100

    Update genesis module to use master branch

--- components/00-genesis/10-genesis.tf
+++ components/00-genesis/10-genesis.tf
@@ -1,5 +1,5 @@
 module "genesis" {
-  source           = "git::https://github.com/hmcts/aks-module-genesis.git?ref=DTSPO-1032-ptlsbox"
+  source           = "git::https://github.com/hmcts/aks-module-genesis.git?ref=master"
   environment      = var.environment
   tags             = module.ctags.common_tags
   developers_group = local.developers_group
