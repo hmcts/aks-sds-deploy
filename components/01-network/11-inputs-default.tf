ee8658ec397d6e8eeff83255eb6f9379e3df9404
msl8r <72858053+msl8r@users.noreply.github.com>
2021-09-10 14:06:56 +0100

    DTSPO-4676 system user pools (#94)
    * DTSPO-4676 - initial changes for system, linux and win pools
    
    * DTSPO-4676 - update default node pool name to system, add linux node pool, make node count and size configurable.
    
    * DTSPO-4676 - tf fmt
    
    * DTSPO-4676 - set empty node taint
    
    * DTSPO-4676 - use branch with default pool name updated to system with taint
    
    * DTSPO-4676 - revert branch back to master
    
    * DTSPO-4676 - use split node pools

--- components/01-network/11-inputs-default.tf
+++ components/01-network/11-inputs-default.tf
@@ -23,40 +23,40 @@ locals {
     nonprod = {
       subscription = "fb084706-583f-4c9a-bdab-949aac66ba5c"
       ukSouth = {
-        name        = "hmcts-hub-nonprodi"
+        name         = "hmcts-hub-nonprodi"
         peering_name = "hubUkS"
-        next_hop_ip = "10.11.72.36"
+        next_hop_ip  = "10.11.72.36"
       }
       ukWest = {
-        name        = "ukw-hub-nonprodi"
+        name         = "ukw-hub-nonprodi"
         peering_name = "hubUkW"
-        next_hop_ip = "10.49.72.36"
+        next_hop_ip  = "10.49.72.36"
       }
     }
     sbox = {
       subscription = "ea3a8c1e-af9d-4108-bc86-a7e2d267f49c"
       ukSouth = {
-        name        = "hmcts-hub-sbox-int"
+        name         = "hmcts-hub-sbox-int"
         peering_name = "hubUkS"
-        next_hop_ip = "10.10.200.36"
+        next_hop_ip  = "10.10.200.36"
       }
       ukWest = {
-        name        = "ukw-hub-sbox-int"
+        name         = "ukw-hub-sbox-int"
         peering_name = "hubUkW"
-        next_hop_ip = "10.48.200.36"
+        next_hop_ip  = "10.48.200.36"
       }
     }
     prod = {
       subscription = "0978315c-75fe-4ada-9d11-1eb5e0e0b214"
       ukSouth = {
-        name        = "hmcts-hub-prod-int"
+        name         = "hmcts-hub-prod-int"
         peering_name = "hubUkS"
-        next_hop_ip = "10.11.8.36"
+        next_hop_ip  = "10.11.8.36"
       }
       ukWest = {
-        name        = "ukw-hub-prod-int"
+        name         = "ukw-hub-prod-int"
         peering_name = "hubUkW"
-        next_hop_ip = "10.49.8.36"
+        next_hop_ip  = "10.49.8.36"
       }
     }
   }
--- components/aks/inputs-default.tf
+++ components/aks/inputs-default.tf
@@ -10,6 +10,20 @@ variable "ptl_cluster" {
   default = false
 }
 
+variable "system_node_pool" {
+  description = "Map to override the system node pool config"
+}
+
+variable "linux_node_pool" {
+  description = "Map to override the linux node pool config"
+
+}
+
+variable "windows_node_pool" {
+  description = "Map to override the windows node pool config"
+
+}
+
 variable "sku_tier" {
   default = "Free"
 }
