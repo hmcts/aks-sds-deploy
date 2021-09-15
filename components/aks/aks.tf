054b4b02f6edce9afd3196453d68770c2fbf665b
Tim Jacomb <21194782+timja@users.noreply.github.com>
2021-09-15 15:17:59 +0100

    Only enable user system node pool on sbox for now

--- components/aks/aks.tf
+++ components/aks/aks.tf
@@ -61,7 +61,7 @@ module "kubernetes" {
   tags     = module.ctags.common_tags
   sku_tier = var.sku_tier
 
-  enable_user_system_nodepool_split = true
+  enable_user_system_nodepool_split = var.environment == "sbox" ? true : false
 
   additional_node_pools = contains(["ptlsbox", "ptl"], var.environment) ? [] : [
     {
