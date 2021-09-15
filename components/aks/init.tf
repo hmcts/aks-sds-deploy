9d3d46a75de512e625c0606a38a6fe236338869b
msl8r <72858053+msl8r@users.noreply.github.com>
2021-09-15 12:10:10 +0100

    update azurerm to 2.75

--- components/aks/init.tf
+++ components/aks/init.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
-      version               = "2.57.0"
+      version               = "2.75.0"
       configuration_aliases = [azurerm.hmcts-control]
     }
   }
