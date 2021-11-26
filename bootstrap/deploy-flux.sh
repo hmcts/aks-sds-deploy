7c1844565ecb82421329510322097d841098a94f
msl8r <72858053+msl8r@users.noreply.github.com>
2021-11-26 09:51:35 +0000

    DTSPO-5581 - enable flux v2 deployment for ptl and sbox (#111)
    * DTSPO-5581 - enable flux v2 deployment for ptl and sbox
    
    * DTSPO-5581 - enable flux v2 deployment for ptl and sbox

--- bootstrap/deploy-flux.sh
+++ bootstrap/deploy-flux.sh
@@ -10,7 +10,6 @@ HELM_OPERATOR_VER="1.4.0"
 flux_repo_list="k8s/environments/${ENVIRONMENT}/cluster-${CLUSTER_NAME}\,k8s/environments/${ENVIRONMENT}/cluster-${CLUSTER_NAME}-overlay\,k8s/environments/${ENVIRONMENT}/common\,k8s/environments/${ENVIRONMENT}/common-overlay\,k8s/common"
 AGENT_BUILDDIRECTORY=/tmp
 
-
 ############################################################
 # Functions
 ############################################################
@@ -51,15 +50,14 @@ EOF
 
 # -----------------------------------------------------------
 
-  ./kustomize build ${TMP_DIR}/admin |  kubectl apply -f -
-  # workaround 'unable to recognize "STDIN": no matches for kind "AzurePodIdentityException" in version "aadpodidentity.k8s.io/v1"'
-  sleep 1
-  kubectl apply -f https://raw.githubusercontent.com/hmcts/shared-services-flux/master/k8s/namespaces/admin/aad-pod-identity/mic-exception.yaml
-  kubectl apply -f https://raw.githubusercontent.com/hmcts/shared-services-flux/master/k8s/namespaces/kube-system/aad-pod-identity/mic-exception.yaml
+    ./kustomize build ${TMP_DIR}/admin |  kubectl apply -f -
+    # workaround 'unable to recognize "STDIN": no matches for kind "AzurePodIdentityException" in version "aadpodidentity.k8s.io/v1"'
+    sleep 1
+    kubectl apply -f https://raw.githubusercontent.com/hmcts/shared-services-flux/master/k8s/namespaces/admin/aad-pod-identity/mic-exception.yaml
+    kubectl apply -f https://raw.githubusercontent.com/hmcts/shared-services-flux/master/k8s/namespaces/kube-system/aad-pod-identity/mic-exception.yaml
 
-  rm -rf ${TMP_DIR}
+    rm -rf ${TMP_DIR}
 }
-
 
 function pod_identity_flux_sop_setup {
     echo "SOPS MI Role"
@@ -75,20 +73,17 @@ function helm_add_repo {
     --namespace admin
 }
 
-
 function helm_apply_crd {
     echo "Kubectl apply fluxcd"
     kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/${HELM_OPERATOR_VER}/deploy/crds.yaml
 }
 
-
-
 function flux_ssh_git_key {
-        echo " Kubectl Create Secret"
-        kubectl create secret generic flux-git-deploy \
-        --from-file=identity=$AGENT_BUILDDIRECTORY/flux-ssh-git-key \
-        --namespace admin \
-        --dry-run=client -o yaml | kubectl apply -f -
+    echo " Kubectl Create Secret"
+    kubectl create secret generic flux-git-deploy \
+    --from-file=identity=$AGENT_BUILDDIRECTORY/flux-ssh-git-key \
+    --namespace admin \
+    --dry-run=client -o yaml | kubectl apply -f -
 }
 
 function flux_install {
@@ -106,6 +101,74 @@ function flux_helm_operator_install {
     --namespace admin
 }
 
+function flux_v2_pod_identity_sops_setup {
+    echo "Creating Pod Identity"
+    cat ../kubernetes/charts/aad-pod-identities/aks-sops-role.yaml | \
+    sed -e 's@MI_RESOURCE_ID@'"$(az identity show --resource-group 'genesis-rg' --name aks-${ENVIRONMENT}-mi --query 'id' | sed 's/"//g')"'@' | \
+    sed -e 's@MI_CLIENTID@'"$(az identity show --resource-group 'genesis-rg' --name aks-${ENVIRONMENT}-mi --query 'clientId' | sed 's/"//g')"'@' | \
+    sed -e 's@admin@flux-system@' > ${TMP_DIR}/gotk/aks-sops-aadpodidentity.yaml
+}
+
+function flux_v2_ssh_git_key {
+    ssh-keyscan -t ecdsa-sha2-nistp256 github.com > $AGENT_BUILDDIRECTORY/known_hosts
+    echo " Kubectl Create Secret"
+    kubectl create secret generic git-credentials \
+    --from-file=identity=$AGENT_BUILDDIRECTORY/flux-ssh-git-key \
+    --from-file=identity.pub=$AGENT_BUILDDIRECTORY/flux-ssh-git-key.pub \
+    --from-file=known_hosts=$AGENT_BUILDDIRECTORY/known_hosts \
+    --namespace flux-system \
+    --dry-run=client -o yaml > ${TMP_DIR}/gotk/git-credentials.yaml
+}
+
+function flux_v2_installation {
+
+    FLUX_CONFIG_URL=https://raw.githubusercontent.com/hmcts/shared-services-flux/master
+    
+
+# -----------------------------------------------------------
+# Deploy components and CRDs
+    (
+    cat <<EOF
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+namespace: flux-system
+resources:
+- ${FLUX_CONFIG_URL}/apps/flux-system/base/gotk-components.yaml
+- git-credentials.yaml
+- aks-sops-aadpodidentity.yaml
+
+patchesStrategicMerge:
+- ${FLUX_CONFIG_URL}/apps/flux-system/base/patches/kustomize-controller-patch.yaml
+EOF
+    ) > "${TMP_DIR}/gotk/kustomization.yaml"
+# -----------------------------------------------------------
+    ./kustomize build ${TMP_DIR}/gotk |  kubectl apply -f -
+
+    # Wait for CRDs to be in an established state
+    kubectl -n flux-system wait --for condition=established --timeout=60s customresourcedefinition.apiextensions.k8s.io/gitrepositories.source.toolkit.fluxcd.io
+    kubectl -n flux-system wait --for condition=established --timeout=60s customresourcedefinition.apiextensions.k8s.io/kustomizations.kustomize.toolkit.fluxcd.io
+# -----------------------------------------------------------
+
+# -----------------------------------------------------------
+# Apply kustomization and gitrepository declarations
+    (
+    cat <<EOF
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+namespace: flux-system
+resources:
+- ${FLUX_CONFIG_URL}/apps/flux-system/base/kustomize.yaml
+- ${FLUX_CONFIG_URL}/apps/flux-system/base/flux-config-gitrepo.yaml
+
+patchesStrategicMerge:
+- ${FLUX_CONFIG_URL}/apps/flux-system/${ENVIRONMENT}/${CLUSTER_NAME}/kustomize.yaml
+EOF
+    ) > "${TMP_DIR}/flux-config/kustomization.yaml"
+# -----------------------------------------------------------
+    ./kustomize build ${TMP_DIR}/flux-config |  kubectl apply -f -
+
+
+}
 
 ############################################################
 # End of functions
@@ -126,3 +189,13 @@ echo "****  flux is now installed ****"
 flux_helm_operator_install
 echo "**** helm operator is now installed ****"
 
+
+FLUX_V2_CLUSTERS=( 'ptl' 'sbox' )
+
+if [[ " ${FLUX_V2_CLUSTERS[*]} " =~ " ${ENVIRONMENT} " ]]; then
+    TMP_DIR=/tmp/flux/${ENVIRONMENT}/${CLUSTER_NAME}
+    mkdir -p $TMP_DIR/{gotk,flux-config}
+    flux_v2_pod_identity_sops_setup
+    flux_v2_ssh_git_key
+    flux_v2_installation
+fi
