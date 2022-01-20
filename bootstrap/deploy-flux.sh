ac6c0a61e8427f7a46653c6c7fe63260a95be39a
Zoe Cope <zoe.cope@hmcts.net>
2022-01-20 12:36:56 +0000

    add ptl to v1

--- bootstrap/deploy-flux.sh
+++ bootstrap/deploy-flux.sh
@@ -194,7 +194,7 @@ if [[ " ${FLUX_V2_CLUSTERS[*]} " =~ " ${ENVIRONMENT} " ]]; then
     flux_v2_installation
 fi
 
-FLUX_V1_CLUSTERS=( 'dev' 'demo' 'ithc' 'prod' 'ptlsbox' 'stg' 'test' )
+FLUX_V1_CLUSTERS=( 'dev' 'demo' 'ithc' 'stg' 'test' 'prod' 'ptlsbox' 'ptl')
 
 if [[ " ${FLUX_V1_CLUSTERS[*]} " =~ " ${ENVIRONMENT} " ]]; then
     pod_identity_components
