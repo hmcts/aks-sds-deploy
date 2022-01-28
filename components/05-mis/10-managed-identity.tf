f187ccad72c88125ab0de7518cd65fbece92e7f8
msl8r <72858053+msl8r@users.noreply.github.com>
2022-01-28 18:11:14 +0000

    DTSPO-6478 - only add permissions if env is demo

--- components/05-mis/10-managed-identity.tf
+++ components/05-mis/10-managed-identity.tf
@@ -21,6 +21,7 @@ resource "azurerm_role_assignment" "Reader" {
 }
 
 resource "azurerm_role_assignment" "public_ip_write" {
+  count                = var.environment == "demo" ? 1 : 0
   principal_id         = azurerm_user_assigned_identity.sops-mi.principal_id
   role_definition_name = "Network Contributor"
   scope                = data.azurerm_resource_group.genesis_rg.name
