335343ffa9132bb710093b120a10ddee9d3d65e9
thomast1906 <thomast@kainos.com>
2022-02-15 14:09:23 +0000

    missing vars

--- components/aks-iam/inputs-required.tf
+++ components/aks-iam/inputs-required.tf
@@ -9,6 +9,12 @@ variable "cluster_count" {}
 # Remote State
 variable "control_vault" {}
 
+variable "builtFrom" {}
+
+variable "location" {
+  default = "uksouth"
+}
+
 variable "service_shortname" {
   default = "aks"
 }
