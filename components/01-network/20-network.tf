fb06c9b2a703ea85e26002f8d3398868a183f6fb
mokainos <72137340+mokainos@users.noreply.github.com>
2022-03-18 15:43:17 +0000

    update

--- components/01-network/20-network.tf
+++ components/01-network/20-network.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source = "git::https://github.com/hmcts/aks-module-network.git?ref=dtspo7364"
+  source = "git::https://github.com/hmcts/aks-module-network.git?ref=master"
 
   resource_group_name = local.network_resource_group_name
 
