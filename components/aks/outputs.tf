449239cb31ea82ad4acda3e5a87dfecaaca00d73
msl8r <72858053+msl8r@users.noreply.github.com>
2022-04-04 09:36:33 +0100

    DTSPO-7029 - remove commented code

--- components/aks/outputs.tf
+++ components/aks/outputs.tf
@@ -1,7 +1,4 @@
 output "clusters" {
-  # value     = join(" ", toset(module.kubernetes[*]).cluster)
-  # value = join(" ", [for cluster in module.kubernetes : cluster[*].cluster[*]])
   value = join(" ", flatten([for cluster in module.kubernetes : cluster[*].cluster]))
-  # value     = module.kubernetes[*]
   sensitive = false
 }
