c5f04c550d212db4c5e42b784139f371c6b4f325
Zoe Cope <zoe.cope@hmcts.net>
2022-04-05 11:56:22 +0100

    undo stuff

--- components/05-mis/01-inputs-required.tf
+++ components/05-mis/01-inputs-required.tf
@@ -5,23 +5,3 @@ variable "project" {}
 variable "control_vault" {}
 
 variable "builtFrom" {}
-
-variable "product" {}
-
-variable "clusters" {
-  type        = list(string)
-  description = "List of clusters to manage e.g [\"00\", \"01\"] "
-  default     = []
-}
-
-variable "service_shortname" {
-  default = "aks"
-}
-
-variable "kubernetes_cluster_ssh_key" {}
-
-variable "kubernetes_cluster_version" {}
-
-variable "availability_zones" {
-  type = list(any)
-}
--- components/05-mis/kubelet-identity.tf
+++ components/05-mis/kubelet-identity.tf
@@ -1,14 +1,8 @@
 resource "azurerm_role_assignment" "Kubelet-MI" {
-  name = "aks-kubelet-${var.environment}-mi"
+  name                 = "aks-kubelet-${var.environment}-mi"
   principal_id         = azurerm_user_assigned_identity.sops-mi.principal_id
   role_definition_name = "Managed Identity Operator"
   scope                = azurerm_user_assigned_identity.sops-mi.id
-}
-
-resource "azurerm_resource_group" "disks_resource_group" {
-  location = var.location
-  name     = "disks-${var.environment}-rg"
-  tags     = module.ctags.common_tags
 }
 
 resource "azurerm_role_assignment" "sbox_registry_acrpull" {
@@ -16,18 +10,6 @@ resource "azurerm_role_assignment" "sbox_registry_acrpull" {
   for_each = local.is_sbox ? toset(var.clusters) : toset([])
   provider             = azurerm.sds_sbox_acr
   role_definition_name = "AcrPull"
-  principal_id         = module.kubernetes[each.value].kubelet_object_id
+  principal_id         = data.azurerm_role_assignment.Kubelet-MI.id
   scope                = data.azurerm_resource_group.sds_sbox_acr[0].id
-}
-
-resource "azurerm_resource_group" "kubernetes_resource_group" {
-  for_each = toset(var.clusters)
-  location = var.location
-
-  name = format("%s-%s-%s-rg",
-    var.project,
-    var.environment,
-    each.value
-  )
-  tags = module.ctags.common_tags
 }
