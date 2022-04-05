8cb095ed839463388b9529ca19e36003e7c27f6f
Zoe Cope <zoe.cope@hmcts.net>
2022-04-05 17:33:19 +0100

    put back provider

--- components/05-mis/00-init.tf
+++ components/05-mis/00-init.tf
@@ -41,3 +41,10 @@ provider "azurerm" {
   features {}
   alias = "log_analytics"
 }
+
+provider "azurerm" {
+  subscription_id            = local.acr["sds_sbox"].subscription
+  skip_provider_registration = "true"
+  features {}
+  alias = "sds_sbox_acr"
+}
