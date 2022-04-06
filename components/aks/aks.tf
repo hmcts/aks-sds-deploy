35b781d57de97335f037b8de1aa890e40bd24ad0
Zoe Cope <zoe.cope@hmcts.net>
2022-04-06 15:20:00 +0100

    more outputs

--- components/aks/aks.tf
+++ components/aks/aks.tf
@@ -55,7 +55,6 @@ module "kubernetes" {
   environment = var.environment
   location    = var.location
   kubelet_uami_enabled = true
-  object_id = azurerm_user_assigned_identity.Kubelet-MI.principal_id
 
 
   providers = {
