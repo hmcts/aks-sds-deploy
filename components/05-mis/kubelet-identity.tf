1aef56d85a57b2a42bdd8b0e5a9386307fc903cf
Zoe Cope <zoe.cope@hmcts.net>
2022-04-06 19:07:21 +0100

    naming

--- components/05-mis/kubelet-identity.tf
+++ components/05-mis/kubelet-identity.tf
@@ -1,15 +1,15 @@
 resource "azurerm_user_assigned_identity" "kubelet_uami" {
-  name                 = "aks-kubelet-${var.environment}-mi"
+  name = "aks-kubelet-${var.environment}-mi"
 
   resource_group_name = data.azurerm_resource_group.genesis_rg.name
   location            = data.azurerm_resource_group.genesis_rg.location
-  tags = local.common_tags
+  tags                = local.common_tags
 }
 
 resource "azurerm_role_assignment" "sbox_registry_acrpull" {
-  for_each = local.is_sbox ? toset(var.clusters) : toset([])
+  for_each             = local.is_sbox ? toset(var.clusters) : toset([])
   provider             = azurerm.sds_sbox_acr
   role_definition_name = "AcrPull"
-  principal_id         = azurerm_user_assigned_identity.kubelet_uami.principal_id
+  principal_id         = azurerm_user_assigned_identity.kubelet_uami.object_id
   scope                = data.azurerm_resource_group.sds_sbox_acr[0].id
 }
