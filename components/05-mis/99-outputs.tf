30589e4e050e2ce01369c4af749e2950ab27b6b4
Zoe Cope <zoe.cope@hmcts.net>
2022-04-07 10:03:20 +0100

    move to true + remove outputs

--- components/05-mis/99-outputs.tf
+++ components/05-mis/99-outputs.tf
@@ -9,15 +9,3 @@ output "mi_id" {
 output "mi_principal_id" {
   value = azurerm_user_assigned_identity.sops-mi.principal_id
 }
-
-// output "kubelet_uami_client_id" {
-//   value = azurerm_user_assigned_identity.kubelet_uami.client_id
-// }
-
-// output "kubelet_uami_id" {
-//   value = azurerm_user_assigned_identity.kubelet_uami.id
-// }
-
-// output "kubelet_uami_principal_id" {
-//   value = azurerm_user_assigned_identity.kubelet_uami.principal_id
-// }
