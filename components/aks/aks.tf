fc741af4f6f3d21d2aadc825eae865f3c727bd5c
Zoe Cope <zoe.cope@hmcts.net>
2022-04-19 15:07:21 +0100

    fmt

--- components/aks/aks.tf
+++ components/aks/aks.tf
@@ -50,13 +50,13 @@ locals {
 
 
 module "kubernetes" {
-  for_each             = toset([for k, v in var.clusters : k])
-  source               = "git::https://github.com/hmcts/aks-module-kubernetes.git?ref=DTSPO-7031_move_node_resources"
-  environment          = var.environment
-  location             = var.location
-  
-  kubelet_uami_enabled = true
-  kubelet_enabled = true
+  for_each    = toset([for k, v in var.clusters : k])
+  source      = "git::https://github.com/hmcts/aks-module-kubernetes.git?ref=DTSPO-7031_move_node_resources"
+  environment = var.environment
+  location    = var.location
+
+  kubelet_uami_enabled     = true
+  kubelet_identity_enabled = true
 
   providers = {
     azurerm               = azurerm
