59ff6c43797799a34d7c8e18bed3d12532a6375b
Zoe Cope <zoe.cope@hmcts.net>
2022-04-19 11:40:02 +0100

    add module refs

--- components/05-mis/kubelet-identity.tf
+++ components/05-mis/kubelet-identity.tf
@@ -6,6 +6,11 @@ resource "azurerm_user_assigned_identity" "kubelet_uami" {
   tags                = local.common_tags
 }
 
+data "azurerm_user_assigned_identity" "aks" {
+  name                = "aks-${var.environment}-mi"
+  resource_group_name = data.azurerm_resource_group.genesis_rg.name
+}
+
 resource "azurerm_role_assignment" "sbox_registry_acrpull" {
   for_each = local.is_sbox ? toset(["sbox"]) : toset([])
   provider             = azurerm.sds_sbox_acr
@@ -14,21 +19,20 @@ resource "azurerm_role_assignment" "sbox_registry_acrpull" {
   scope                = data.azurerm_resource_group.sds_sbox_acr[0].id
 }
 
-
 resource "azurerm_role_assignment" "genesis_managed_identity_operator" {
-  principal_id         = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].object_id
+  principal_id         = module.azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].object_id
   scope                = data.azurerm_user_assigned_identity.aks.id
   role_definition_name = "Managed Identity Operator"
 }
 
 resource "azurerm_role_assignment" "uami_rg_identity_operator" {
-  principal_id         = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].object_id
+  principal_id         = module.azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].object_id
   scope                = data.azurerm_resource_group.managed-identity-operator.id
   role_definition_name = "Managed Identity Operator"
 }
 
 resource "azurerm_role_assignment" "node_infrastructure_update_scale_set" {
-  principal_id         = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].object_id
+  principal_id         = module.azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].object_id
   scope                = data.azurerm_resource_group.node_resource_group.id
   role_definition_name = "Virtual Machine Contributor"
 }
