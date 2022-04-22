32a2d785cb0a214921d84fc1c307cdd566dd8562
Tim Jacomb <tim.jacomb@hmcts.net>
2022-04-22 18:07:37 +0100

    Kubelet needs Managed identity Operator

--- components/05-mis/kubelet-identity.tf
+++ components/05-mis/kubelet-identity.tf
@@ -13,3 +13,10 @@ resource "azurerm_role_assignment" "sbox_registry_acrpull" {
   principal_id         = azurerm_user_assigned_identity.kubelet_uami.principal_id
   scope                = data.azurerm_resource_group.sds_sbox_acr[0].id
 }
+
+resource "azurerm_role_assignment" "kubelet_uami_genesis" {
+  # DTS Bootstrap Principal_id
+  principal_id         = azurerm_user_assigned_identity.kubelet_uami.principal_id
+  role_definition_name = "Managed Identity Operator"
+  scope                = azurerm_user_assigned_identity.sops-mi.id
+}
