f18d49d298c1f52798c4b2634ac03cceda57d9b7
Zoë <54357991+zc-hmcts@users.noreply.github.com>
2022-05-09 11:21:24 +0100

    DTSPO-7031 Enable Bring your own identity for AKS worker nodes (#180)
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>
    Co-authored-by: Enda Kelly <enda.kelly@hmcts.net>
    Co-authored-by: Tim Jacomb <tim.jacomb@hmcts.net>

--- components/05-mis/application-managed-identity-rg.tf
+++ components/05-mis/application-managed-identity-rg.tf
@@ -3,3 +3,9 @@ resource "azurerm_resource_group" "application-mi" {
   location = var.location
   tags     = local.common_tags
 }
+
+resource "azurerm_role_assignment" "uami_rg_identity_operator" {
+  principal_id         = azurerm_user_assigned_identity.kubelet_uami.principal_id
+  scope                = azurerm_resource_group.application-mi.id
+  role_definition_name = "Managed Identity Operator"
+}
