0798a6beeb64bee648e2dc9974ee44dede19a676
Zoe Cope <zoe.cope@hmcts.net>
2022-05-11 10:23:10 +0100

    remove comment

--- components/05-mis/00-init.tf
+++ components/05-mis/00-init.tf
@@ -26,7 +26,6 @@ provider "azurerm" {
   subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
 }
 
-// TODO delete after applying MI in all ENVs
 // working around 'Error: Provider configuration not present'
 provider "azurerm" {
   subscription_id            = 5ca62022-6aa2-4cee-aaa7-e7536c8d566c
