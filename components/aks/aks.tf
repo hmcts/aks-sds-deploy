cab8b7cb465ae8e0cbcc98678dc9e336c5fec8ad
Tim Jacomb <timjacomb1@gmail.com>
2022-10-04 14:22:23 +0100

    Enable OIDC issuer

--- components/aks/aks.tf
+++ components/aks/aks.tf
@@ -51,7 +51,7 @@ locals {
 
 module "kubernetes" {
   for_each    = toset([for k, v in var.clusters : k])
-  source      = "git::https://github.com/hmcts/aks-module-kubernetes.git?ref=master"
+  source      = "git::https://github.com/hmcts/aks-module-kubernetes.git?ref=enable-oidc-issuer"
   environment = var.environment
   location    = var.location
 
