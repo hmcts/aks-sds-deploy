b02cb466258f0149cc225862394a3b70b3a7629d
louisehuyton <louise.huyton@justice.gov.uk>
2022-10-20 17:01:59 +0100

    Update Terraform azurerm to v3.27.0

--- modules/vnet_peering/init.tf
+++ modules/vnet_peering/init.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
-      version               = "3.24.0"
+      version               = "3.27.0"
       configuration_aliases = [azurerm.initiator, azurerm.target]
     }
   }
