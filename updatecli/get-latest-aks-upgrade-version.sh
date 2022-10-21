776fd47656df80519673835c5f58c15fb6b3a875
Jay Walaszek <jayw@kainos.com>
2022-10-21 13:55:05 +0100

    Add missing sort

--- updatecli/get-latest-aks-upgrade-version.sh
+++ updatecli/get-latest-aks-upgrade-version.sh
@@ -7,5 +7,6 @@ aks_subscription=$3
 az aks get-upgrades \
     --name $aks_name \
     --resource-group $aks_resource_group \
-    --subscription $aks_subscription | \
-    jq -r 'if (.controlPlaneProfile.upgrades == null) then .controlPlaneProfile.kubernetesVersion else .controlPlaneProfile.upgrades|map(select(.isPreview == null).kubernetesVersion)| .[] end'
+    --subscription $aks_subscription \
+    | jq -r 'if (.controlPlaneProfile.upgrades == null) then .controlPlaneProfile.kubernetesVersion else .controlPlaneProfile.upgrades|map(select(.isPreview == null).kubernetesVersion)| .[] end' \
+    | sort --reverse --version-sort | head --lines 1
