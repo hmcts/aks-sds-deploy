dba4ee9f0b3d222c27019ee457da8139512f8fc5
Jay W <40270505+JayDoubleu@users.noreply.github.com>
2022-10-21 10:59:15 +0100

    Test upgrade aks version updatecli loops (#277)

---
+++ updatecli/get-latest-aks-upgrade-version.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+aks_name=$1
+aks_resource_group=$2
+aks_subscription=$3
+
+az aks get-upgrades \
+    --name $aks_name \
+    --resource-group $aks_resource_group \
+    --subscription $aks_subscription \
+    --query 'controlPlaneProfile.upgrades[?isPreview==null].kubernetesVersion' \
+    --output tsv | sort --reverse --version-sort | head --lines 1
