efcb333712c9c2702cac19107aa197d61a19f05c
Jay Walaszek <jayw@kainos.com>
2022-10-21 13:58:23 +0100

    Make JQ multiline for better readability

--- updatecli/get-latest-aks-upgrade-version.sh
+++ updatecli/get-latest-aks-upgrade-version.sh
@@ -8,5 +8,9 @@ az aks get-upgrades \
     --name $aks_name \
     --resource-group $aks_resource_group \
     --subscription $aks_subscription \
-    | jq -r 'if (.controlPlaneProfile.upgrades == null) then .controlPlaneProfile.kubernetesVersion else .controlPlaneProfile.upgrades|map(select(.isPreview == null).kubernetesVersion)| .[] end' \
+    | jq -r '
+    if (.controlPlaneProfile.upgrades == null) then
+        .controlPlaneProfile.kubernetesVersion
+    else .controlPlaneProfile.upgrades|map(select(.isPreview == null).kubernetesVersion)
+    | .[] end' \
     | sort --reverse --version-sort | head --lines 1
