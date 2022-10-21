32e26cae2f9eef3b4be97cd084a17ff6a81b4bb3
Jay Walaszek <jayw@kainos.com>
2022-10-21 09:25:33 +0100

    Use subscription name instead of id when connecting to clusters

--- updatecli/get-latest-aks-upgrade-version.sh
+++ updatecli/get-latest-aks-upgrade-version.sh
@@ -2,11 +2,11 @@
 
 aks_name=$1
 aks_resource_group=$2
-aks_subscription_id=$3
+aks_subscription=$3
 
 az aks get-upgrades \
     --name $aks_name \
     --resource-group $aks_resource_group \
-    --subscription $aks_subscription_id \
+    --subscription $aks_subscription \
     --query 'controlPlaneProfile.upgrades[?isPreview==null].kubernetesVersion' \
     --output tsv
