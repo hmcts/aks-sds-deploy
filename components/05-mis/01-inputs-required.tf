019c82f174f1152440f3ca86abc2c5aa621470a2
Louise Huyton <louise.huyton@justice.gov.uk>
2022-11-29 16:55:30 +0000

    try add clusters variable

--- components/05-mis/01-inputs-required.tf
+++ components/05-mis/01-inputs-required.tf
@@ -7,3 +7,18 @@ variable "control_vault" {}
 variable "builtFrom" {}
 
 variable "product" {}
+
+variable "clusters" {
+  type        = map(map(string))
+  description = <<-EOF
+Map of clusters to manage. Example:
+clusters = {
+  "00" = {
+    kubernetes_version = "1.22.6"
+  },
+  "01" = {
+    kubernetes_version = "1.22.6"
+  }
+}
+EOF
+}
