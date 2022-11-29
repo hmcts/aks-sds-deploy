842ac21331f5074a930c85bab455ea0771efd2b0
Louise Huyton <louise.huyton@justice.gov.uk>
2022-11-29 15:52:08 +0000

    remove quotation

--- components/05-mis/03-interpolated-defaults.tf
+++ components/05-mis/03-interpolated-defaults.tf
@@ -19,7 +19,7 @@ data "azurerm_resource_group" "sds_sbox_acr" {
 
 data "azurerm_kubernetes_cluster" "aks_cluster" {
   name                = "ss-sbox-00-aks"
-  resource_group_name = "module.kubernetes.kubernetes_resource_group"
+  resource_group_name = module.kubernetes.kubernetes_resource_group
 }
 
 data "azurerm_key_vault" "genesis_keyvault" {
