7e68a12ea7ae2a9df90962bb0b11ead34792bdd3
Louise Huyton <louise.huyton@justice.gov.uk>
2022-12-07 14:56:32 +0000

    Try fix linting

--- components/aks/aks.tf
+++ components/aks/aks.tf
@@ -110,11 +110,11 @@ module "kubernetes" {
 }
 
 module "ctags" {
-  source        = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
-  environment   = var.env
-  product       = var.product
-  builtFrom     = var.builtFrom
-  autoShutdown  = var.autoShutdown
+  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment = var.env
+  product     = var.product
+  builtFrom   = var.builtFrom
+  autoShutdown = var.autoShutdown
 }
 
 
