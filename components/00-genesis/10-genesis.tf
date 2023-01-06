82be4d033e2070b6ffef9ea21eeabebd4461a901
Matt Slater <72858053+msl8r@users.noreply.github.com>
2023-01-06 09:33:09 +0000

    DTSPO-11790 - add expires tag (#352)
    * DTSPO-11790 - add expires tag
    
    * DTSPO-11790 - add expires tag to all components

--- components/00-genesis/10-genesis.tf
+++ components/00-genesis/10-genesis.tf
@@ -6,8 +6,9 @@ module "genesis" {
 }
 
 module "ctags" {
-  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
-  environment = var.env
-  product     = var.product
-  builtFrom   = var.builtFrom
+  source       = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment  = var.env
+  product      = var.product
+  builtFrom    = var.builtFrom
+  expiresAfter = var.expiresAfter
 }
