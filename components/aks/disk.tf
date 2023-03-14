13e841d5ecafdf8e7cec560e3c5a973a1a131dbd
Tim Jacomb <timjacomb1@gmail.com>
2023-03-14 14:12:14 +0000

    Restore old disk resource name

--- components/aks/disk.tf
+++ components/aks/disk.tf
@@ -1,5 +1,5 @@
 # TODO this file would be better in the MI component but that would require a lot of state migration
-resource "azurerm_resource_group" "disks" {
+resource "azurerm_resource_group" "disks_resource_group" {
   location = var.location
   name     = "disks-${var.env}-rg"
   tags     = module.ctags.common_tags
@@ -8,5 +8,5 @@ resource "azurerm_resource_group" "disks" {
 resource "azurerm_role_assignment" "disk" {
   principal_id         = data.azurerm_user_assigned_identity.aks.principal_id
   role_definition_name = "Virtual Machine Contributor"
-  scope                = azurerm_resource_group.disks.id
+  scope                = azurerm_resource_group.disks_resource_group.id
 }
