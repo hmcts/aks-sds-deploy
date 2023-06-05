7c30c5dc21e354b6ddd7633d000caa3d0688a883
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-06-05 12:29:31 +0100

    use branch of network module

--- components/01-network/20-network.tf
+++ components/01-network/20-network.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source = "git::https://github.com/hmcts/aks-module-network.git?ref=master"
+  source = "git::https://github.com/hmcts/aks-module-network.git?ref=postgres-service-endpoints-2"
 
   resource_group_name = local.network_resource_group_name
 
