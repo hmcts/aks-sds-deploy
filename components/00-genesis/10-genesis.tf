28a1710ce80e9d080e05d084fa0d8ae3a3f33b83
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-06-09 12:18:09 +0100

    add missing var

--- components/00-genesis/10-genesis.tf
+++ components/00-genesis/10-genesis.tf
@@ -3,6 +3,7 @@ module "genesis" {
   environment      = var.env
   tags             = module.ctags.common_tags
   developers_group = local.developers_group
+  business_area    = lower(module.ctags.common_tags["businessArea"])
 }
 
 module "ctags" {
