a274b8495db95572ae9c3c159338a53962a49027
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2023-08-22 10:55:32 +0100

    fix typo

--- bootstrap/deploy-flux.sh
+++ bootstrap/deploy-flux.sh
@@ -161,8 +161,8 @@ cat <<EOF
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 resources:
-  - https://github.com/hmcts/sds-flux-config//apps/azureserviceoperator-system/cert-manager/
-  - https://github.com/hmcts/sds-flux-config//apps/azureserviceoperator-system/aso/
+  - https://github.com/hmcts/sds-flux-config/apps/azureserviceoperator-system/cert-manager/
+  - https://github.com/hmcts/sds-flux-config/apps/azureserviceoperator-system/aso/
   - https://raw.githubusercontent.com/hmcts/sds-flux-config/master/apps/azureserviceoperator-system/${ENVIRONMENT}/base/aso-controller-settings.yaml
 EOF
 ) > "${TMP_DIR}/kustomization.yaml"
