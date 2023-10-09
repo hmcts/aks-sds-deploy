1f86ed23a0b66e460367f8325f03cf4a509b937c
Daniel Wilson <daniel.wilson@kainos.com>
2023-10-09 16:10:27 +0100

    add block for rg role assignments for ASO with dev to stg translation

--- components/05-mis/10-managed-identity.tf
+++ components/05-mis/10-managed-identity.tf
@@ -62,6 +62,46 @@ locals {
       "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/dev.platform.hmcts.net"
     ])
   }
+  wi_environment_rg    = var.env == "dev" ? "stg" : var.env
+
+  # MIs for managed-identities-sbox-rg etc - for workload identity with ASO
+  mi_sds = {
+    # DTS-SHAREDSERVICES-SBOX
+    sbox = {
+      subscription_id = "a8140a9e-f1b0-481f-a4de-09e2ee23f7ab"
+    }
+    # DTS-SHAREDSERVICES-STG
+    dev = {
+      subscription_id = "74dacd4f-a248-45bb-a2f0-af700dc4cf68"
+    }
+    stg = {
+      subscription_id = "74dacd4f-a248-45bb-a2f0-af700dc4cf689"
+    }
+    # DTS-SHAREDSERVICES-ITHC
+    ithc = {
+      subscription_id = "ba71a911-e0d6-4776-a1a6-079af1df7139"
+    }
+    # DTS-SHAREDSERVICES-TEST
+    test = {
+      subscription_id = "3eec5bde-7feb-4566-bfb6-805df6e10b90"
+    }
+    # DTS-SHAREDSERVICES-DEMO
+    demo = {
+      subscription_id = "c68a4bed-4c3d-4956-af51-4ae164c1957c"
+    }
+    # DTS-SHAREDSERVICES-PROD
+    prod = {
+      subscription_id = "5ca62022-6aa2-4cee-aaa7-e7536c8d566c"
+    }
+    # DTS-SHAREDSERVICESPTL-SBOX
+    ptlsbox = {
+      subscription_id = "64b1c6d6-1481-44ad-b620-d8fe26a2c768"
+    }
+    # DTS-SHAREDSERVICESPTL
+    ptl = {
+      subscription_id = "6c4d2513-a873-41b4-afdd-b05a33206631"
+    }
+  }
 }
 
 resource "azurerm_role_assignment" "externaldns-dns-zone-contributor" {
@@ -92,4 +132,11 @@ resource "azurerm_role_assignment" "service_operator" {
   principal_id         = azurerm_user_assigned_identity.sops-mi.principal_id
   role_definition_name = "Contributor"
   scope                = data.azurerm_subscription.current.id
+}
+
+resource "azurerm_role_assignment" "service_operator_workload_identity" {
+  count                = var.service_operator_settings_enabled ? 1 : 0
+  principal_id         = data.azurerm_user_assigned_identity.aks.principal_id
+  role_definition_name = "Contributor"
+  scope                = "/subscriptions/${local.mi_sds[var.env].subscription_id}/resourceGroups/managed-identities-${local.wi_environment_rg}-rg"
 }
