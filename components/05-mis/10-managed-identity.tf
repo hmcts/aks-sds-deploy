8d8afd4268740758d93d20023e266597bddff380
Rees Pozzi <reespozzi@icloud.com>
2023-10-13 13:55:25 +0100

    Give role assignment also to admin-mi

--- components/05-mis/10-managed-identity.tf
+++ components/05-mis/10-managed-identity.tf
@@ -52,6 +52,7 @@ resource "azurerm_key_vault_access_policy" "sops-policy" {
 }
 
 resource "azurerm_role_assignment" "acme-vault-access" {
+  for_each             = toset([azurerm_user_assigned_identity.sops-mi.principal_id, azurerm_user_assigned_identity.wi-admin-mi.principal_id])
   scope                = data.azurerm_key_vault.acme.id
   role_definition_name = "Key Vault Secrets User"
   principal_id         = azurerm_user_assigned_identity.sops-mi.principal_id
