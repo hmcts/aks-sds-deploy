e067cebb416d8c9ab3d98972c5648577cbaea327
Rees Pozzi <rees.pozzi@justice.gov.uk>
2023-10-16 12:47:11 +0100

    Add missing role assignments back

--- components/05-mis/10-managed-identity.tf
+++ components/05-mis/10-managed-identity.tf
@@ -136,6 +136,14 @@ resource "azurerm_role_assignment" "genesis_managed_identity_operator" {
   role_definition_name = "Managed Identity Operator"
 }
 
+# Adds missing role assignment for demo and dev for azure service operator
+resource "azurerm_role_assignment" "service_operator" {
+  count                = var.env == "dev" || var.env == "demo" ? 1 : 0
+  principal_id         = azurerm_user_assigned_identity.sops-mi.principal_id
+  role_definition_name = "Contributor"
+  scope                = data.azurerm_subscription.current.id
+}
+
 # Gives dev access to stg resource group
 resource "azurerm_role_assignment" "service_operator_workload_identity" {
   count                = var.env == "dev" ? 1 : 0
