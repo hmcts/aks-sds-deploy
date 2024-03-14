5d3a0ca4a61169c9c3397169f0acf885b48ee85e
Joshua Glass <joshua.glass@justice.gov.uk>
2024-03-14 15:56:24 +0000

    removed block testing inputs

--- components/aks/aks.tf
+++ components/aks/aks.tf
@@ -139,14 +139,12 @@ module "kubernetes" {
 
   enable_node_os_channel_upgrade_nodeimage = contains(["sbox"], var.env) ? true : false
 
-  maintenance_window_node_os {
-    duration    = var.node_os_maintenance_window_duration
-    frequency   = var.node_os_maintenance_window_frequency
-    day_of_week = var.node_os_maintenance_window_day_of_week
-    interval    = var.node_os_maintenance_window_interval
-    start_time  = var.node_os_maintenance_window_start_time
-    utc_offset  = var.node_os_maintenance_window_utc_offset
-  }
+  duration    = var.node_os_maintenance_window_duration
+  frequency   = var.node_os_maintenance_window_frequency
+  day_of_week = var.node_os_maintenance_window_day_of_week
+  interval    = var.node_os_maintenance_window_interval
+  start_time  = var.node_os_maintenance_window_start_time
+  utc_offset  = var.node_os_maintenance_window_utc_offset
 }
 
 module "ctags" {
