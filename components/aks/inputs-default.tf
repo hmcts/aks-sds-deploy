ff42b5869f2c14744ebac26403e417a6c6d3bcd3
Joshua Glass <joshua.glass@justice.gov.uk>
2024-03-19 16:02:29 +0000

    updated defaults

--- components/aks/inputs-default.tf
+++ components/aks/inputs-default.tf
@@ -66,40 +66,40 @@ variable "monitor_diagnostic_setting_metrics" {
   default = false
 }
 
-variable "node_os_maintenance_window_duration" {
-  description = "Duration of maintenance window in hours"
-  default     = 4
-}
+# variable "node_os_maintenance_window_duration" {
+#   description = "Duration of maintenance window in hours"
+#   default     = 4
+# }
 
-variable "node_os_maintenance_window_frequency" {
-  description = "Frequency of maintenance window, repeat every day, week or month"
-  default     = "Daily"
-}
+# variable "node_os_maintenance_window_frequency" {
+#   description = "Frequency of maintenance window, repeat every day, week or month"
+#   default     = "Daily"
+# }
 
-variable "node_os_maintenance_window_interval" {
-  description = "The interval for maintenance runs"
-  default     = 1
-}
+# variable "node_os_maintenance_window_interval" {
+#   description = "The interval for maintenance runs"
+#   default     = 1
+# }
 
-variable "node_os_maintenance_window_day_of_week" {
-  description = "The day of the week for the maintenance run. Required in combination with weekly frequency."
-  default     = null
-}
+# variable "node_os_maintenance_window_day_of_week" {
+#   description = "The day of the week for the maintenance run. Required in combination with weekly frequency."
+#   default     = null
+# }
 
-variable "node_os_maintenance_window_start_time" {
-  description = "Start time of maintenance run format HH:mm"
-  default     = null
-}
+# variable "node_os_maintenance_window_start_time" {
+#   description = "Start time of maintenance run format HH:mm"
+#   default     = null
+# }
 
-variable "node_os_maintenance_window_utc_offset" {
-  description = "Used to adjust time zome of start time, +/-HH:mm"
-  default     = null
-}
+# variable "node_os_maintenance_window_utc_offset" {
+#   description = "Used to adjust time zome of start time, +/-HH:mm"
+#   default     = null
+# }
 
-variable "node_os_maintenance_window_start_date" {
-  description = "Date when maintenance window will start"
-  default     = null
-}
+# variable "node_os_maintenance_window_start_date" {
+#   description = "Date when maintenance window will start"
+#   default     = null
+# }
 
 variable "node_os_maintenance_window_config" {
   type = object({
@@ -112,10 +112,10 @@ variable "node_os_maintenance_window_config" {
     start_date  = optional(string)
   })
   default = {
-    frequency  = "Daily"
+    frequency  = "Weekly"
     interval   = 1
     duration   = 4
-    start_date = null
+    day_of_week = "Monday"
     utc_offset = null
     start_date = null
   }
