5301bacf98afac999d7b8cfdfc00cbe75dbf4b87
Daniel Wilson <102226203+danielwilsonkainos@users.noreply.github.com>
2024-04-29 17:08:49 +0100

    DTSPO-15799: Refactor pipeline to run sequentially on all enviroments (#560)
    * trigger pipeline
    
    * push to test
    
    * indent
    
    * quote
    
    * questionable formatting
    
    * parameter object
    
    * parameter object
    
    * parameter object
    
    * parameter object
    
    * parameter object
    
    * dependsOn conditional
    
    * dependsOn conditional
    
    * dependsOn conditional
    
    * dependsOn conditional
    
    * dependsOn conditional
    
    * dependsOn conditional
    
    * dependsOn conditional
    
    * dependsOn conditional
    
    * dependsOn to stagger sbox, stg, prod
    
    * dependsOn to stagger sbox, stg, prod
    
    * dependsOn to stagger sbox, stg, prod
    
    * reorder triggers
    
    * reorder triggers
    
    * reorder triggers
    
    * more dependsOn
    
    * more dependsOn
    
    * dependsOn remove conditional for stages
    
    * remove service connection from parameter object
    
    * change display names
    
    * update vars/params passed to stages
    
    * update name
    
    * update bootstrap
    
    * upper & kv access policy update
    
    * add master branch trigger
    
    * edit pipeline
    
    * add pr trigger
    
    * edit pipeline
    
    * edit pipeline
    
    * remove variable from aks stage
    
    * add depend on to prod env
    
    * add depend on to prod env
    
    * revert condition in pipeline
    
    * fix quotes
    
    * fix quotes
    
    * fix quotes
    
    * fix double quote issue
    
    * fix double quote issue
    
    * fix pipeline condition and double quote issue
    
    * fix quote issue
    
    * remove depend on network stage
    
    * fix typo
    
    * edit pipeline
    
    * delete azure-pipeline-environments.yaml
    
    * edit agent pool to latest
    
    * edit pipeline checking cluster stage
    
    ---------
    
    Co-authored-by: nicholas chang <nicholasc001@hotmail.com>
    Co-authored-by: Nicholas Chang <60232417+NickAzureDevops@users.noreply.github.com>

---
+++ scripts/checking-clusters.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Generic query for name of clusters in subscription, checks for 00 and 01 and stores output in variable
+cluster00="$(az aks list --output tsv --query '[].[name]' | grep 00 2>/dev/null)"
+cluster01="$(az aks list --output tsv --query '[].[name]' | grep 01 2>/dev/null)"
+
+# Check which values where found, if 00 and 01 then multiple clusters
+# If 00 and not 01, single 00 cluster found
+# If not 00 and 01, single 01 cluster found
+# if neither 00 or 01, no clusters found
+if [[ -n $cluster00 && -n $cluster01 ]]; then 
+    echo "Found multiple clusters"
+    output="All"
+elif [[ -n $cluster00 && ! -n $cluster01 ]]; then
+    echo "Found 00 cluster only"
+    output="00"
+elif [[ ! -n $cluster00 && -n $cluster01 ]]; then
+    echo "Found 01 cluster only"
+    output="01"
+else 
+    echo "Found no clusters"
+    output="None"
+fi
+
+#Create pipeline variable for use later in script
+echo "##vso[task.setvariable variable=cluster_deploy;isOutput=true]$output"
