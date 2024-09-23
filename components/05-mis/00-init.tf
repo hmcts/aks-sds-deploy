6919c65d6b5962ef34e3f27230f8c19f2c205001
Rees Pozzi <reespozzi@icloud.com>
2024-09-23 14:14:42 +0100

    Terraform fmt

--- components/05-mis/00-init.tf
+++ components/05-mis/00-init.tf
@@ -17,7 +17,7 @@ variable "subscription_id" {}
 provider "azurerm" {
   features {}
   skip_provider_registration = "true"
-  subscription_id = var.subscription_id
+  subscription_id            = var.subscription_id
 }
 
 provider "azurerm" {
