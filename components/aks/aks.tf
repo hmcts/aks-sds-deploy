8aea991e2ce148c3b02f184025cf230e11daae7c
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2024-10-17 10:14:33 +0100

    DTSPO-19027 Updating settings on automatic cluster (#640)
    * updating settings on automatic cluster due to pipeline failing
    
    * fixing format
    
    * fixing format
    
    * fixing  autoscaler format

--- components/aks/aks.tf
+++ components/aks/aks.tf
@@ -294,10 +294,34 @@ resource "azapi_resource" "managedCluster" {
         }
       ]
       networkProfile = {
-        networkPlugin = "azure"
-        dnsServiceIP  = "10.0.0.10"
-        serviceCidr   = "10.0.0.0/16"
-        outboundType  = "loadBalancer"
+        networkPlugin    = "azure"
+        networkDataplane = "azure"
+        dnsServiceIP     = "10.0.0.10"
+        serviceCidr      = "10.0.0.0/16"
+        loadBalancerSku  = "Standard"
+        outboundType     = "loadBalancer"
+      }
+      autoScalerProfile = {
+        balance-similar-node-groups           = "false"
+        daemonset-eviction-for-empty-nodes    = "false"
+        daemonset-eviction-for-occupied-nodes = "true"
+        expander                              = "random"
+        ignore-daemonsets-utilization         = "false"
+        max-empty-bulk-delete                 = "10"
+        max-graceful-termination-sec          = "600"
+        max-node-provision-time               = "15m"
+        max-total-unready-percentage          = "45"
+        new-pod-scale-up-delay                = "0s"
+        ok-total-unready-count                = "3"
+        scale-down-delay-after-add            = "10m"
+        scale-down-delay-after-delete         = "10s"
+        scale-down-delay-after-failure        = "3m"
+        scale-down-unneeded-time              = "10m"
+        scale-down-unready-time               = "20m"
+        scale-down-utilization-threshold      = "0.5"
+        scan-interval                         = "10s"
+        skip-nodes-with-local-storage         = "false"
+        skip-nodes-with-system-pods           = "true"
       }
     }
     sku = {
