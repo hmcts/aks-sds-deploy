1517627f4d34fe3fe3fecf1be69ccb7b80bd4c39
Rees Pozzi <reespozzi@icloud.com>
2024-11-15 12:18:44 +0000

    Update working directory instead:

--- bootstrap/apply-default-rbac.sh
+++ bootstrap/apply-default-rbac.sh
@@ -3,6 +3,6 @@ set -e
 az_keyvault_name="${4}"
 
 echo "Applying Default Rbac"
-cat aks-sds-deploy/kubernetes/rbac/default-rbac.yaml | \
+cat ../kubernetes/rbac/default-rbac.yaml | \
 sed -e 's@${CLUSTER_READ_ONLY_GROUP}@'"$(az keyvault secret show --vault-name ${az_keyvault_name}  --name 'aks-user-rbac-group-id' --query 'value' -o tsv)"'@'  | \
         kubectl apply -f -
