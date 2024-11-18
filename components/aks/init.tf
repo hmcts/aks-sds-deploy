8b656871f48b8df9b996fb4443a39643f5355ea3
Jake Moorhead <jake.moorhead@justice.gov.uk>
2024-11-18 13:33:30 +0000

    Updating azurerm to 4.9.0

--- components/aks/init.tf
+++ components/aks/init.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
-      version               = "3.78.0"
+      version               = "4.9.0"
       configuration_aliases = [azurerm.hmcts-control]
     }
     azapi = {
