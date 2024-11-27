a85a1e9553c2fcbaf53067415fbf7fd671a23ed5
Jake Moorhead <jake.moorhead@justice.gov.uk>
2024-11-27 10:20:06 +0000

    Bumping back to 4.9.0 after fixing tf plan changes.

--- components/aks/init.tf
+++ components/aks/init.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
-      version               = "4.0.0"
+      version               = "4.9.0"
       configuration_aliases = [azurerm.hmcts-control]
     }
     azapi = {
