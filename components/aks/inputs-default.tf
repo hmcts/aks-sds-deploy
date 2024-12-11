0505ed16384aaf47dbfd35bb2ec303036dfce624
Nicholas Chang <60232417+NickAzureDevops@users.noreply.github.com>
2024-12-11 12:59:59 +0000

    Dtspo 22631 - custom settings per aks cluster on SDS  (#658)
    * add custom settings per aks cluster
    
    * edit test and stg tfvars
    
    * edit test and stg tfvars
    
    * uncomment backend block
    
    * fix issues with system node pool and csi driver and tfvars
    
    * comment out backend remote
    
    * edit aks.tf
    
    * edit tfvars
    
    * remove loop and add foreach
    
    * format code
    
    * edit sbox tfvars
    
    * refactor code
    
    * revert demo,prod,ptl,stg and edit test.tfvars
    
    * comment out prod env on pipeline
    
    * comment out 00 on dev
    
    * edit tfvars
    
    * edit tfvars
    
    * comment out prod stages
    
    * format code
    
    * edit tfvars version name
    
    * edit system node pool size
    
    * edit tfvars
    
    * format code
    
    * edit logic in aks components
    
    * format code
    
    * edit logic in local
    
    * format code
    
    * edit logic in local
    
    * edit logic in local
    
    * add min node to system node pool in cluster vars
    
    * add min node to system node pool in cluster vars
    
    * format code
    
    * edit aks.tf
    
    * comment out backend block
    
    * edit system sku
    
    * edit max node for system node pool
    
    * edit sbox max pods for linux
    
    * edit sbox max pods for linux
    
    * uncommented out unwanted cluster
    
    * edit additional node pool logic
    
    * fix logic in additional nodep pool
    
    * fix logic in additional nodep pool
    
    * revert changes from additional node pool
    
    * edit loop in additional node pool
    
    * re-add msising data source
    
    * edit tfvars values
    
    * edit tfvars values
    
    * fix demo, dev and prod values
    
    * edit stg value
    
    * edit maangedcluster loop
    
    * format code
    
    * edit ssh key on sbox
    
    * format code
    
    * comment out other env in pipeline

--- components/aks/inputs-default.tf
+++ components/aks/inputs-default.tf
@@ -18,16 +18,6 @@ variable "sku_tier" {
   default = "Standard"
 }
 
-variable "system_node_pool" {
-  description = "Map to override the system node pool config"
-  default     = {}
-}
-
-variable "linux_node_pool" {
-  description = "Map to override the linux node pool config"
-  default     = {}
-}
-
 variable "windows_node_pool" {
   description = "Map to override the windows node pool config"
   default     = {}
--- components/aks/inputs-required.tf
+++ components/aks/inputs-required.tf
@@ -7,37 +7,97 @@ variable "builtFrom" {}
 
 variable "product" {}
 
-# Remote State
+# # Remote State
 variable "control_vault" {}
 
 # Kubernetes
-variable "kubernetes_cluster_ssh_key" {}
-
-variable "kubernetes_cluster_agent_min_count" {
-  default = 1
-}
-variable "kubernetes_cluster_agent_max_count" {
-  default = 3
-}
-variable "kubernetes_cluster_agent_vm_size" {
-  default = "Standard_D4ds_v5"
-}
-
-variable "availability_zones" {
-  type = list(any)
-}
-
 variable "clusters" {
-  type        = map(map(string))
   description = <<-EOF
-Map of clusters to manage. Example:
-clusters = {
-  "00" = {
-    kubernetes_version = "1.22.6"
-  },
-  "01" = {
-    kubernetes_version = "1.22.6"
-  }
+    Map of clusters to manage. Example:
+    clusters = {
+      "00" = {
+        kubernetes_cluster_version = "1.22.6"
+        kubernetes_cluster_ssh_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCe..."
+        enable_automatic_channel_upgrade_patch = true
+
+        system_node_pool = {
+          vm_size   = "Standard_D4ds_v5"
+          min_nodes = 2
+          max_nodes = 4
+        }
+
+        linux_node_pool = {
+          vm_size   = "Standard_D4ds_v5"
+          min_nodes = 4
+          max_nodes = 10
+        }
+
+        windows_node_pool = {
+          vm_size   = "Standard_D4ds_v5"
+          min_nodes = 2
+          max_nodes = 4
+        }
+
+        availability_zones = ["1"]
+        autoShutdown       = true
+
+        node_os_maintenance_window_config = {
+          frequency  = "Daily"
+          start_time = "16:00"
+          is_prod    = false
+        }
+      },
+      "01" = {
+        kubernetes_cluster_version = "1.30"
+        kubernetes_cluster_ssh_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCe..."
+        enable_automatic_channel_upgrade_patch = true
+
+        system_node_pool = {
+          min_nodes = 2
+          max_nodes = 4
+        }
+
+        linux_node_pool = {
+          vm_size   = "Standard_D4ds_v5"
+          max_nodes = 10
+          max_nodes = 10
+        }
+
+        availability_zones = ["1"]
+        autoShutdown       = true
+
+        node_os_maintenance_window_config = {
+          frequency  = "Daily"
+          start_time = "16:00"
+          is_prod    = false
+        }
+      }
+    }
+  EOF
+  type = map(object({
+    kubernetes_cluster_version             = string
+    kubernetes_cluster_ssh_key             = string
+    enable_automatic_channel_upgrade_patch = optional(bool, false)
+    system_node_pool = object({
+      min_nodes = number
+      max_nodes = number
+    })
+
+    linux_node_pool = object({
+      max_nodes = number
+      max_pods  = number
+    })
+
+    node_os_maintenance_window_config = object({
+      frequency  = string
+      start_time = string
+      is_prod    = bool
+    })
+
+    availability_zones = list(string)
+  }))
 }
-EOF
+
+variable "csi_driver_enabled" {
+  default = true
 }
