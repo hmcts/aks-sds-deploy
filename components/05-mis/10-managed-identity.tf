d5c880fa0a57e275aa1d9bacdda1e1c39a9f7d35
anne-moore <Anne.Moore@justice.gov.uk>
2025-03-10 16:36:45 +0000

    Giving dev MI contributor access to genesis-rg

--- components/05-mis/10-managed-identity.tf
+++ components/05-mis/10-managed-identity.tf
@@ -163,3 +163,11 @@ resource "azurerm_role_assignment" "service_operator_workload_identity" {
   role_definition_name = "Contributor"
   scope                = "/subscriptions/${local.mi_sds[var.env].subscription_id}"
 }
+
+# Gives dev access to genesis resource group
+resource "azurerm_role_assignment" "genesis_rg_contributor" {
+  count                = var.env == "dev" ? 1 : 0
+  principal_id         = azurerm_user_assigned_identity.sops-mi.principal_id
+  role_definition_name = "Contributor"
+  scope                = data.azurerm_resource_group.genesis_rg.id
+}
