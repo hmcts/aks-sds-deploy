d83c517dba95c8dc70a3ebcbee02a7407605907d
anne-moore <Anne.Moore@justice.gov.uk>
2025-03-11 16:43:51 +0000

    Retrigger build and updating comment

--- components/05-mis/10-managed-identity.tf
+++ components/05-mis/10-managed-identity.tf
@@ -164,7 +164,7 @@ resource "azurerm_role_assignment" "service_operator_workload_identity" {
   scope                = "/subscriptions/${local.mi_sds[var.env].subscription_id}"
 }
 
-# Gives dev access to genesis resource group
+# Gives dev access to genesis resource group for WI
 resource "azurerm_role_assignment" "genesis_rg_contributor" {
   count                = var.env == "dev" ? 1 : 0
   principal_id         = azurerm_user_assigned_identity.sops-mi.principal_id
