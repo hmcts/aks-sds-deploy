ce7d0886db943c48d2348f00a4fd9890c081afbe
Dawid Strozak <dawid.strozak@hmcts.net>
2025-06-05 17:41:02 +0100

    Improve phrasing of a log in a script

--- bootstrap/create-github-app-secret.sh
+++ bootstrap/create-github-app-secret.sh
@@ -12,7 +12,7 @@ if [[ -z $flux_github_app_id ]] || [[ -z $flux_github_app_installation_id ]] || 
             echo "GitHub App secrets not found in Key Vault, please ensure GitHub App secrets for this environment are set in its Key Vault"
             exit 1
     else
-            echo "Flux GitHub App secrets Download"
+            echo "Downloading Flux GitHub App secrets from Key Vault"
             az keyvault secret download --name flux-github-app-id --vault-name ${az_keyvault_name} --file $AGENT_BUILDDIRECTORY/flux-github-app-id --encoding ascii
             az keyvault secret download --name flux-github-app-installation-id --vault-name ${az_keyvault_name} --file $AGENT_BUILDDIRECTORY/flux-github-app-installation-id --encoding ascii
             az keyvault secret download --name flux-github-app-priv-key --vault-name ${az_keyvault_name} --file $AGENT_BUILDDIRECTORY/flux-github-app-private-key --encoding ascii
